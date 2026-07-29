685ac10132d4261120846f86a6b2b37759066e6d
timeoberault-jpg <timeo.berault@gmail.com>
2026-07-29 20:57:57 +0200

    Update client.ts

--- lib/resend/client.ts
+++ lib/resend/client.ts
@@ -1,7 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
+const resend = new Resend(process.env.RESEND_API_KEY || "re_placeholder");
 export async function sendNewReviewAlert(params: {
   to: string;
   businessName: string;
