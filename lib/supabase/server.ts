b2fef32c8d315f87f28200a5ec7633231089dc66
timeoberault-jpg <timeo.berault@gmail.com>
2026-07-29 20:45:49 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,8 +12,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
-          try {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {          try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
